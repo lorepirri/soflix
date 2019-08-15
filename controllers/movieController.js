1653bb13c02eca42a7067fa3026cc8d12a789d44
Lorenzo <6698585+lorepirri@users.noreply.github.com>
2019-08-15 15:32:09 +0200

    add movie, user controllers

---
+++ controllers/movieController.js
@@ -0,0 +1,34 @@
+const Movies = require('../models/movies');
+// this is just included to trigger registering the Actors schema
+const Actors = require('../models/actors');
+
+// Get all movies
+module.exports.movies_list = function(req, res) {
+  Movies.find()
+    .populate('Actors')
+    .then( movies => {
+      res.status(201).json(movies);
+    })
+    .catch( error => {
+      console.error(error);
+      res.status(500).send(`Error <${error}>`);
+    });
+};
+
+// Get a movie by Title
+module.exports.movie_get = function(req, res) {
+  let { Title } = req.params;
+  Movies.findOne({ Title: {$regex: `${Title}.*`, $options: 'i'} })
+    .populate('Actors')
+    .then( movie => {
+      if (movie) {
+        res.json(movie);
+      } else {
+        res.status(404).send(`Movie <${Title}> not found.`);
+      }
+    })
+    .catch( err => {
+      console.error(err);
+      res.status(500).send(`Error <${err}>`);
+    });  
+};
---
+++ controllers/userController.js
@@ -0,0 +1,178 @@
+const Users = require('../models/users');
+
+// Get all users
+module.exports.user_list = function(req, res) {
+  Users.find()
+    .then( users => {
+      res.status(201).json(users);
+    })
+    .catch( error => {
+      console.error(error);
+      res.status(500).send(`Error <${error}>`);
+    });
+};
+
+// Get a user by Username
+module.exports.user_get = function(req, res) {
+  let { Username } = req.params;
+  Users.findOne({ Username })
+    .populate('FavoriteMovies')
+    .exec( (err, user) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send(`Error <${err}>`);
+      }      
+      else if (user) {
+        res.json(user);
+      } else {
+        res.status(404).send(`User ${Username} not found.`);
+      }
+    });
+};
+
+// Adds data for a new user to the list of Users.
+/* We’ll expect JSON in this format
+{
+  Username: String,
+  (required)
+  Password: String,
+  (required)
+  Email: String,
+  (required)
+  Birthday: Date
+}*/
+module.exports.user_create = (req, res) => {
+  let { Username, Password, Email, Birthday } = req.body;
+  Users.findOne({ Username })
+    .then( user => {
+      if (user) {
+        return res.status(400).send(`User ${Username} already exists.`);
+      } else {
+        Users.create({
+          Username,
+          Password,
+          Email,
+          Birthday
+        })
+          .then( user => {res.status(201).json(user);})
+          .catch( error => {
+            console.error(error);
+            res.status(500).send(`Error <${error}>.`);
+          });
+      }
+    })
+    .catch( error => {
+      console.error(error);
+      res.status(500).send(`Error <${error}>.`);
+    });
+};
+
+// Update a user's info, by username
+/* We’ll expect JSON in this format
+{
+  Username: String,
+  (required)
+  Password: String,
+  (required)
+  Email: String,
+  (required)
+  Birthday: Date
+}*/
+module.exports.user_update = (req, res) => {
+  let { Username, Password, Email, Birthday } = req.body;
+  Users.findOneAndUpdate(
+    { Username: req.params.Username }, 
+    { $set: {
+      Username,
+      Password,
+      Email,
+      Birthday
+    }},
+    { new: true}) // pass the updated document to the callback
+    .then( updatedUser => {
+      // return the updated user
+      res.json(updatedUser);
+    })
+    .catch( err => {
+      console.error(err);
+      res.status(500).send(`Error ${err}`);
+    });
+};
+  
+// Delete user by Username
+module.exports.user_delete = (req, res) => {
+  let { Username } = req.params;
+  Users.findOneAndRemove({ Username })
+    .then( user => {
+      if (user) {
+        res.status(201).send(`${Username} was deleted.`);
+      } else {
+        res.status(404).send(`${Username} was not found.`);
+      }
+    })
+    .catch( err => {
+      console.error(err);
+      res.status(500).send(`Error <${err}>.`);
+    });
+};
+
+// Add a favorite Movie to a User.
+module.exports.user_add_favorite_movie = (req, res) => {
+  let { Username, MovieID } = req.params;
+  Users.findOneAndUpdate({ Username }, {
+    $push : { FavoriteMovies : MovieID }
+  },
+  { new : true }) // Pass the updated document to the callback
+    .then( updatedUser => {
+      res.json(updatedUser);
+    })
+    .catch( err => {
+      console.error(err);
+      res.status(500).send(`Error: <${err}>.`);
+    });
+};
+
+// Remove a favorite Movie to a User.
+module.exports.user_remove_favorite_movie = (req, res) => {
+  let { Username, MovieID } = req.params;
+  Users.findOneAndUpdate({ Username }, {
+    $pull : { FavoriteMovies : MovieID }
+  },
+  { new : true }) // Pass the updated document to the callback
+    .then( updatedUser => {
+      res.json(updatedUser);
+    })
+    .catch( err => {
+      console.error(err);
+      res.status(500).send(`Error: <${err}>.`);
+    });
+};
+
+// app.post('/users/:id/:movie_id', (req, res) => {
+//   let user = Users.find((user) => { return user.id === req.params.id; });
+//   let movie = Movies.find((movie) => { return movie.id === req.params.movie_id; });
+
+//   if (user && movie) {
+//     user.favorites = [...new Set([...user.favorites, req.params.movie_id])];
+//     res.status(201).send(user);
+//   } else if (!movie) {
+//     res.status(404).send('Movie with id ' + req.params.movie_id + ' was not found.');
+//   } else {
+//     res.status(404).send('User with id ' + req.params.id + ' was not found.');
+//   }
+// });
+
+// // remove a favorite Movie from a User.
+// app.delete('/users/:id/:movie_id', (req, res) => {
+//   let user = Users.find((user) => { return user.id === req.params.id; });
+//   let movie = Movies.find((movie) => { return movie.id === req.params.movie_id; });
+
+//   if (user && movie) {
+//     user.favorites = user.favorites.filter((movie_id) => { return movie_id !== req.params.movie_id; });
+//     res.status(201).send(user);
+//   } else if (!movie) {
+//     res.status(404).send('Movie with id ' + req.params.movie_id + ' was not found.');
+//   } else {
+//     res.status(404).send('User with id ' + req.params.id + ' was not found.');
+//   }
+// });
