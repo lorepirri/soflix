acd57b2fcff4fe06c9e488cba55df0f01d91aa5c
Lorenzo <6698585+lorepirri@users.noreply.github.com>
2019-08-17 00:21:31 +0200

    change port for heroku deploy

--- index.js
+++ index.js
@@ -82,6 +82,9 @@ app.use(function(err, req, res, next) {
   res.render('Something broke!');
 });
 
-app.listen(8080, () => {
-  console.log('Soflix is listening on port 8080');
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', function() {
+  console.log(`Soflix is listening on port ${port}`);
 });
