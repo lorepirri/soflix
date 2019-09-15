5340cd4d5f88b031966aafb9a230958a7d65ccbb
Lorenzo <6698585+lorepirri@users.noreply.github.com>
2019-09-15 11:50:21 +0200

    chores add log error, remove log

--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -59,11 +59,10 @@ export function RegistrationView(props) {
       })
       .then(response => {
         const data = response.data;
-        console.log(data);
         window.open('/', '_self'); // the second argument '_self' is necessary so that the page will open in the current tab
       })
       .catch(e => {
-        console.log('error registering the user')
+        console.log('error registering the user', e)
       });      
     }
     // notify that fields were validated,
