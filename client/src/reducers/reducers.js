c3378e80dc07938373ba17c9dcac2e94ba2dc0a9
Lorenzo <6698585+lorepirri@users.noreply.github.com>
2019-09-22 10:07:06 +0200

    add sortFilter reducer

--- client/src/reducers/reducers.js
+++ client/src/reducers/reducers.js
@@ -1,10 +1,19 @@
 import { combineReducers } from 'redux';
 
-import { SET_FILTER, SET_MOVIES, SET_USER } from '../actions/actions';
+import { SET_FILTER, SET_MOVIES, SET_USER, SET_SORT_FILTER } from '../actions/actions';
 
 function visibilityFilter(state = '', action) {
   switch(action.type) {
     case SET_FILTER:
+      return action.value;
+    default:
+      return state;
+  };
+}
+
+function sortFilter(state = '', action) {
+  switch(action.type) {
+    case SET_SORT_FILTER:
       return action.value;
     default:
       return state;
@@ -31,6 +40,7 @@ function userProfile(state = null, action) {
 
 const moviesApp = combineReducers({
   visibilityFilter,
+  sortFilter,
   movies,
   userProfile
 });
